31aab76f64ddcf165735d744d83060be7a774c70
chamodpw2000 <mailtochamodw@gmail.com>
2025-11-01 17:19:37 +0530

    fix: authentication issue fix

--- src/services/PartnershipAgreementsService.js
+++ src/services/PartnershipAgreementsService.js
@@ -1,11 +1,12 @@
-import { TMApi } from "../api/calls"
+import { CopilotApi } from "../api/copilotAPI"
+
 
 const url = '/api/admin'
 
 class PartnershipAgreementsService {
   // Get all event organizers with pagination
   getAllEventOrganizers(page = 0, limit = 10) {
-    return TMApi.get(`${url}/partners`, {
+    return CopilotApi.get(`${url}/partners`, {
       params: {
         page,
         limit
@@ -15,12 +16,12 @@ class PartnershipAgreementsService {
 
   // Get a single event organizer by ID
   getEventOrganizer(id) {
-    return TMApi.get(`${url}/partners/${id}`)
+    return CopilotApi.get(`${url}/partners/${id}`)
   }
   
     // Get all event organizers with pagination
     getAllEventOrganizersRequests(page = 0, limit = 10) {
-      return TMApi.get(`${url}/partner-requests`, {
+      return CopilotApi.get(`${url}/partner-requests`, {
         params: {
           page,
           limit
@@ -31,14 +32,14 @@ class PartnershipAgreementsService {
   
   // Update event organizer status
   updateEventOrganizerStatus(id, status) {
-    return TMApi.put(`${url}/partners/${id}/status`, {
+    return CopilotApi.put(`${url}/partners/${id}/status`, {
       status
     })
   }
 
   // Delete an event organizer
   deleteEventOrganizer(id) {
-    return TMApi.delete(`${url}/partners/${id}`)
+    return CopilotApi.delete(`${url}/partners/${id}`)
   }
 }
 
